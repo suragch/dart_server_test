09b9e9e8359a463ae11e4a00c5341cc370c6f292
suragch <studymongolian@gmail.com>
2019-05-09 13:37:59 -0700

    first commit

---
+++ lib/testingapi.dart
@@ -0,0 +1,11 @@
+/// testingapi
+///
+/// A Aqueduct web server.
+library testingapi;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
